e339ed0974156dee282f4ef8f44436043b240c88
Joshua Vaage <jvaage@protonmail.com>
2020-02-25 21:43:47 -0800

    Update dashboardConfig.js

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -13,13 +13,13 @@ export default {
               sites: [
                 {
                   buildHookId: '5e55fe898c582ee57cb4619a',
-                  title: 'Sanity Studio',
+                  title: 'Admin',
                   name: 'eleventy-sanity-studio',
                   apiId: 'cfc9212a-0800-4bfa-b44c-f4886df6cc8a'
                 },
                 {
                   buildHookId: '5e55fe891036fd80fd975124',
-                  title: 'Blog Website',
+                  title: 'Website',
                   name: 'eleventy-sanity',
                   apiId: '97556dd6-367c-4cda-8524-673ceedf38b7'
                 }
@@ -40,7 +40,7 @@ export default {
     {name: 'project-users', layout: {height: 'auto'}},
     {
       name: 'document-list',
-      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
+      options: {title: 'Recently posted', order: '_createdAt desc', types: ['post']},
       layout: {width: 'medium'}
     }
   ]
