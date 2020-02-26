f1ade7d3abaf2c2b6f665f3fcad8089238697e43
Joshua <joshuavaage@gmail.com>
2020-02-25 21:13:49 -0800

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,53 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-eleventy-blog'
+      }
+    },
+    {name: 'structure-menu'},
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '5e55fe898c582ee57cb4619a',
+                  title: 'Sanity Studio',
+                  name: 'eleventy-sanity-studio',
+                  apiId: 'cfc9212a-0800-4bfa-b44c-f4886df6cc8a'
+                },
+                {
+                  buildHookId: '5e55fe891036fd80fd975124',
+                  title: 'Blog Website',
+                  name: 'eleventy-sanity',
+                  apiId: '97556dd6-367c-4cda-8524-673ceedf38b7'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/whaleen/eleventy-sanity',
+            category: 'Code'
+          },
+          {title: 'Frontend', value: 'https://eleventy-sanity.netlify.com', category: 'apps'}
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.SANITY_PROJECT_ID || 'vyla5lq3',
+    dataset: process.env.SANITY_DATASET || 'production'
+  }
+}
